95eb078610b2512fe1fecf9e3f23f06ec5bc14f5
Eran Kampf <eran@ekampf.com>
2015-07-13 19:27:37 -0700

    NDB arguments

---
+++ webapp2_requestparser/arguments_ndb.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+__author__ = 'ekampf'
+
+import base64
+
+try:
+    from google.appengine.ext import ndb
+except ImportError:
+    raise Exception("NDB Required")
+
+
+class EntityIDArgument(object):
+    def __init__(self, kind, key_only=False, is_base64=False):
+        self.kind = kind
+        self.key_only = key_only
+        self.is_base64 = is_base64
+
+    def __call__(self, entity_id):
+        if self.is_base64:
+            entity_id = base64.urlsafe_b64decode(str(entity_id))
+
+        if self.key_only:
+            return ndb.Key(self.kind, entity_id)
+        else:
+            return ndb.Key(self.kind, entity_id).get()
+
+
+class EntityLongIDArgument(EntityIDArgument):
+    def __call__(self, entity_id):
+        return super(EntityLongIDArgument, self).__call__(long(entity_id))
+
+
+
